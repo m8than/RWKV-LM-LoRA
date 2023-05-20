bd75fe60dfad9cad0340f20417cb8c480f950a67
Nathan <me@nwilce.co.uk>
2023-05-20 02:37:25 +0000

    fix index finding code optimisation

--- RWKV-v4neo/train.py
+++ RWKV-v4neo/train.py
@@ -387,7 +387,7 @@ if __name__ == "__main__":
         return zip(*padded_sequences)
     
     def my_collate_fn(batch):
-        return list(pad_zip(*batch), pad_value=torch.tensor(-1))
+        return list(pad_zip(*batch, pad_value=torch.tensor(-1)))
 
     # must set shuffle=False, persistent_workers=False (because worker is in another thread)
     data_loader = DataLoader(train_data, shuffle=False, pin_memory=True, batch_size=args.micro_bsz, num_workers=1, persistent_workers=False, drop_last=True)
