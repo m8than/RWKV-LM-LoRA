c25b224e82090e18695f92f89d4e98e43478641b
Nathan <me@nwilce.co.uk>
2023-05-21 03:13:40 +0000

    testing

--- RWKV-v4neo/src/registry.py
+++ RWKV-v4neo/src/registry.py
@@ -1,7 +1,9 @@
 class Registry:
     def __init__(self):
+        print("Registry init")
         pass
     def setup(self):
+        print("Registry setup")
         self.last_token_lengths = []
         self.last_ctx_length = 0
         self.total_documents = 0
