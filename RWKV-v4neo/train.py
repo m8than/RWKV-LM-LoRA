49f13a68c76c8ba1d7e89868600f2520027d3a3c
Nathan <me@nwilce.co.uk>
2023-05-21 01:10:56 +0000

    testing

--- RWKV-v4neo/train.py
+++ RWKV-v4neo/train.py
@@ -377,14 +377,12 @@ if __name__ == "__main__":
             batch = sequences[j]
             new_batch = []
             for i in range(len(batch)):
-                length = max(int(batch[i].size(0)) for bch in sequences)
+                length = max(int(bch[i].size(0)) for bch in sequences)
                 new_batch.append(torch.cat((batch[i], torch.zeros(length, dtype=batch[i].dtype))))
             new_sequences.append(new_batch)
-        return tuple(new_sequences)
+        return zip(tuple(new_sequences))
     
     def my_collate_fn(batch):
-        print(batch)
-        print(pad_zip(*batch))
         return pad_zip(*batch)
 
     # must set shuffle=False, persistent_workers=False (because worker is in another thread)
